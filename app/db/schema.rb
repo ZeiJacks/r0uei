a69fd48e4a8bbe7efca5def7bac108f6a1322cec
yu1hpa <yu.1hpa@gmail.com>
2022-12-17 01:15:36 +0900

    chore(users): Add user_id

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_12_074236) do
   create_table "users", force: :cascade do |t|
+    t.string "user_id"
     t.string "username"
     t.string "passwd"
     t.string "email"
